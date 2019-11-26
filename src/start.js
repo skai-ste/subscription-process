5bfd40954394077f8630325fac14ee221aa50ba8
skaiste <25135643+skaisteauguste@users.noreply.github.com>
2019-11-26 22:40:04 +0100

    Added starting state for first page

--- src/start.js
+++ src/start.js
@@ -5,7 +5,18 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import reduxPromise from "redux-promise";
 import reducer from "./reducers";
-const store = createStore(reducer, applyMiddleware(reduxPromise));
+
+const startingState = {
+    duration: "12 months",
+    size_gb: "5",
+    upfront_payment: "no"
+};
+
+const store = createStore(
+    reducer,
+    startingState,
+    applyMiddleware(reduxPromise)
+);
 
 let elem = (
     <Provider store={store}>
