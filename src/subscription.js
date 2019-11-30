cd2ae31d6fdf8625112a5bfb35067ad39d493937
skaiste <25135643+skaisteauguste@users.noreply.github.com>
2019-11-30 21:33:08 +0100

    Adding material-ui styling on first component

--- src/subscription.js
+++ src/subscription.js
@@ -4,17 +4,36 @@ import { Redirect, browserHistory } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "./axios";
 import { setDuration, setAmountOfGb, setUpfrontPayment } from "./actions";
+import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import NativeSelect from "@material-ui/core/NativeSelect";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles(theme => ({
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2)
+    }
+}));
 
 export function Subscription() {
     const dispatch = useDispatch();
     const state = useSelector(state => state);
     console.log("state", state);
+    const classes = useStyles();
 
     return (
         <div>
-            <h1>Select :</h1>
-            <form>
-                <select
+            <h1>Subscription order process in 4 steps:</h1>
+            <FormControl className={classes.formControl}>
+                <InputLabel htmlFor="age-native-helper">Duration</InputLabel>
+                <NativeSelect
                     value={state.duration}
                     name="type"
                     placeholder="type"
@@ -26,9 +45,12 @@ export function Subscription() {
                     <option value="3">3 months</option>
                     <option value="6">6 months</option>
                     <option value="12">12 months</option>
-                </select>
-
-                <select
+                </NativeSelect>
+                <FormHelperText>Select period</FormHelperText>
+            </FormControl>
+            <FormControl className={classes.formControl}>
+                <InputLabel htmlFor="age-native-helper">GB</InputLabel>
+                <NativeSelect
                     value={state.size_gb}
                     name="type"
                     placeholder="type"
@@ -43,9 +65,16 @@ export function Subscription() {
                     <option value="20">20</option>
                     <option value="30">30</option>
                     <option value="50">50</option>
-                </select>
-
-                <select
+                </NativeSelect>
+                <FormHelperText>
+                    Select amount of gigabytes in a cloud
+                </FormHelperText>
+            </FormControl>
+            <FormControl className={classes.formControl}>
+                <InputLabel htmlFor="age-native-helper">
+                    Upfront payment
+                </InputLabel>
+                <NativeSelect
                     value={state.upfront_payment}
                     name="type"
                     placeholder="type"
@@ -56,11 +85,12 @@ export function Subscription() {
                 >
                     <option value="true">yes</option>
                     <option value="false">no</option>
-                </select>
-            </form>
-            <div>
+                </NativeSelect>
+                <FormHelperText>Select payment method</FormHelperText>
+            </FormControl>
+            <Button variant="contained">
                 <Link to="/info">NEXT</Link>
-            </div>
+            </Button>
         </div>
     );
 }
